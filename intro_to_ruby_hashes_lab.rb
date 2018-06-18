1dd301ebe72ca474556ccdc4ef19c5ff4a3c64fc
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-18 00:35:50 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,10 @@ def monopoly_with_fourth_tier
 	  :pieces => 4,
 	  :names => 
 	  {
-	    :reading_railroad => {},
+	    :reading_railroad => 
+	    {
+	      "mortgage_value" => "$100"
+	    },
 	    :pennsylvania_railroad => {},
 	    :b_and_o_railroad => {},
 	    :shortline => {}
